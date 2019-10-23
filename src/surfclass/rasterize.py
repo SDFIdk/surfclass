b7795c97cd479451bf4132771d4c020a3c3e9d08
Nicolai Mogensen <nicolaimogensen@outlook.dk>
2019-10-23 16:03:41 +0200

    Black lint

---
+++ src/surfclass/rasterize.py
@@ -0,0 +1,11 @@
+class Rasterizer:
+    def __init__(self, arg1):
+        self.arg1 = arg1
+
+
+def main():
+    r = Rasterizer()
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,6 +2,7 @@ from click.testing import CliRunner
 from surfclass.scripts.cli import cli
 
 
+
 def test_cli():
     runner = CliRunner()
     result = runner.invoke(cli, ["--version"])
