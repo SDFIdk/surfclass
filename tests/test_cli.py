a2972cbd196b6990387ecd440e93201df7f45fb9
AsgerPetersen <asgerpetersen@gmail.com>
2019-10-30 21:15:57 +0100

    Add pytest conftest with fixtures

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,18 +1,13 @@
-from click.testing import CliRunner
 import surfclass
 from surfclass.scripts.cli import cli
 
 
-def test_cli():
-    runner = CliRunner()
-    result = runner.invoke(cli, ["--version"])
-
+def test_cli(cli_runner):
+    result = cli_runner.invoke(cli, ["--version"])
     assert result.exit_code == 0
 
 
-def test_cli_version():
-    runner = CliRunner()
-    result = runner.invoke(cli, ["--version"])
-
+def test_cli_version(cli_runner):
+    result = cli_runner.invoke(cli, ["--version"])
     expected = f"surfclass, version {surfclass.__version__}\n"
     assert result.output == expected
