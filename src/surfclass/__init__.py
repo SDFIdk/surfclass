632eb104f39eb13213616478702a81ca908a552f
AsgerPetersen <asgerpetersen@gmail.com>
2019-11-06 16:39:07 +0100

    Rasterize use own gridding code

--- src/surfclass/__init__.py
+++ src/surfclass/__init__.py
@@ -1,6 +1,10 @@
+from collections import namedtuple
+
 __version__ = "0.0.1"
 __description__ = "Processes Lidar-Data into a surface classified raster"
 __author__ = "Asger Skovbo Petersen"
 __email__ = "asger@septima.dk"
 __uri__ = "https://github.com/septima/surfclass"
 __license__ = "Licensed under the MIT license"
+
+Bbox = namedtuple("Bbox", ["xmin", "ymin", "xmax", "ymax"])
--- src/surfclass/rasterize.py
+++ src/surfclass/rasterize.py
@@ -1,67 +1,89 @@
 import os
 import json
 import logging
-import pprint
 import pdal
+from surfclass import lidar, rasterwriter, Bbox
 
-pp = pprint.PrettyPrinter(indent=4)
 logger = logging.getLogger(__name__)
+
+dimension_nodata = {
+    "Z": -999,
+    "Intensity": 0,
+    "ReturnNumber": 0,
+    "NumberOfReturns": 0,
+    "Classification": 255,
+    "ScanAngleRank": -999,
+    "Pulse width": -999,
+    "Amplitude": -999,
+}
 
 
 class LidarRasterizer:
-    def __init__(self, resolution, bbox, lidar, dimensions, filterexp):
+    def __init__(self, resolution, bbox, lidarfile, dimensions, filterexp):
         self.resolution = resolution
-        self.bbox = bbox
-        self.lidar = lidar
+        self.bbox = Bbox(*bbox)
+        self.lidar = lidarfile
         self.dimensions = self._validate_dimensions(dimensions)
-        self.reader = self._create_pipeline_reader(lidar)
+        self.reader = self._create_pipeline_reader(lidarfile)
         self.filterexp = filterexp
 
     def start(self):
-        # Collect a writer for each dimension
-        writers = [
-            self._create_pipeline_writer(dim, self.resolution, self.bbox)
-            for dim in self.dimensions
-        ]
-
+        xmin, ymin, xmax, ymax = self.bbox
+        logger.warning("Filtering away everything but ground")
         rangefilter = {
             "type": "filters.range",
             "limits": "Classification[2:2]",  # Ground classification
         }
+        # xmin and ymax are inclusive, xmax and ymin are inclusive. Otherwise out gridsampler crashes
+        boundsfilter = {
+            "type": "filters.crop",
+            "bounds": f"([{xmin}, {xmax - 0.00001}], [{ymin + 0.00001}, {ymax}])",
+        }
 
         # Build the pipeline by concating the reader, filter and writers
-        pipeline_dict = {"pipeline": [self.reader, rangefilter] + writers}
+        pipeline_dict = {"pipeline": [self.reader, boundsfilter, rangefilter]}
+        pipeline_json = json.dumps(pipeline_dict)
+        logger.debug("Using pipeline: %s", pipeline_json)
 
         # Convert the pipeline to stringified JSON (required by PDAL)
-        pipeline = pdal.Pipeline(json.dumps(pipeline_dict))
+        pipeline = pdal.Pipeline(pipeline_json)
 
         if pipeline.validate():
             pipeline.loglevel = 8  # really noisy
             pipeline.execute()
 
         else:
-            print("Pipeline not valid")
+            logger.error("Pipeline not valid")
+
+        logger.debug("Reading data")
+        data = pipeline.arrays
+        logger.debug("Data read: %s", data)
+
+        # For now just assume one array
+        points = data[0]
+
+        # For now get rid of PulseWidth==2.55
+        logger.warning("Dropping returns with pulsewidth >= 2.55")
+        points = points[points[:]["Pulse width"] < 2.55]
+
+        sampler = lidar.GridSampler(points, self.bbox, self.resolution)
+        origin = (xmin, ymax)
+
+        for dim in self.dimensions:
+            nodata = dimension_nodata[dim]
+            outfile = self._output_filename(dim)
+            grid = sampler.make_grid(dim, nodata, masked=False)
+            rasterwriter.write_to_file(
+                outfile, grid, origin, self.resolution, 25832, nodata=nodata
+            )
 
     @classmethod
-    def _create_pipeline_reader(cls, lidar):
-        reader = {"type": "readers.las", "filename": lidar}
-        return reader
+    def _create_pipeline_reader(cls, lidarfile):
+        return {"type": "readers.las", "filename": lidarfile}
 
     @classmethod
-    def _create_pipeline_writer(cls, dimension, resolution, bbox):
-        # Convert the bbox from list [xmin,xmax,ymin,ymax] to: ([xmin,xmax],[ymin,ymax])
-        bbox = str((bbox[0:2], bbox[2:4]))
-        writer = {
-            "type": "writers.gdal",
-            "filename": "1km_6184_720_terrain_" + dimension.replace(" ", "") + ".tif",
-            "resolution": resolution,
-            "dimension": dimension,
-            # "bounds": bbox,
-            "gdaldriver": "GTiff",
-            "gdalopts": "tiled=yes,compress=lzw,predictor=3",
-            "output_type": "mean",
-        }
-        return writer
+    def _output_filename(cls, dimension):
+        return "1km_6184_720_terrain_" + dimension.replace(" ", "") + ".tif"
 
     @classmethod
     def _validate_dimensions(cls, dimensions):
@@ -84,10 +106,13 @@ class LidarRasterizer:
 def test():
     """ Only used for internal testing """
     resolution = 0.5  # Coarse resolution for fast testing
-    bbox = [666000, 6666000, 667000, 6667000]
-    lidar = os.path.join("testdata", "1km_6184_720.las")
+    bbox = Bbox(727000, 6171000, 728000, 6172000)
+    lidarfile = os.path.join(
+        "/Volumes/GoogleDrive/My Drive/Septima - Ikke synkroniseret/Projekter/SDFE/Befæstelse/data/trænings_las",
+        "1km_6171_727.las",
+    )
     dimensions = ["Intensity", "Amplitude", "Pulse width"]
-    r = LidarRasterizer(resolution, bbox, lidar, dimensions, "")
+    r = LidarRasterizer(resolution, bbox, lidarfile, dimensions, "")
 
     r.start()
 
