75a5efc4e53f16e2ea335f0aad50a0b17ab8caaf
AsgerPetersen <asgerpetersen@gmail.com>
2019-11-11 16:00:30 +0100

    Tests for cli lidargrid

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,11 +3,11 @@ from surfclass.scripts.cli import cli
 
 
 def test_cli(cli_runner):
-    result = cli_runner.invoke(cli, ["--version"])
+    result = cli_runner.invoke(cli, ["--version"], catch_exceptions=False)
     assert result.exit_code == 0
 
 
 def test_cli_version(cli_runner):
-    result = cli_runner.invoke(cli, ["--version"])
+    result = cli_runner.invoke(cli, ["--version"], catch_exceptions=False)
     expected = f"surfclass, version {surfclass.__version__}\n"
     assert result.output == expected
