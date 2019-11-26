0c5abc1dfcaef7e8c48dd1b5301268898492b017
AsgerPetersen <asgerpetersen@gmail.com>
2019-11-26 13:56:45 +0100

    cli extract add usage

--- src/surfclass/scripts/extract.py
+++ src/surfclass/scripts/extract.py
@@ -58,6 +58,30 @@ def stats(
     lco,
     classraster,
 ):
+    r"""Count occurences of cell values inside polygons.
+
+    This tool counts the ocurrences of specified cell values whose cell center falls inside a polygon and adds the
+    counts as feature attributes.
+
+    An attribute per reported class is added. The attribute "class_0" reports the number of cells with the value 0
+    within the polygon and a "total_count" attribute reports the total number of cells within the polygon.
+
+    Vector operations are carreid out using the OGR library and as such datasources, layers and associated creation
+    options follow the semantics specified by OGR. See https://gdal.org.
+
+    Output dataset and output layer may exist beforehand in which case the tool tries to open it in append mode.
+
+    Classrange defines which cell values are reported individually. Cell values not in the range will still count
+    towards the total_count.
+
+    An optional bbox can be specified to limit the features read from input datasource. Otherwise the tool will
+    use the bbox of the raster file.
+
+    If --clip is specified the geometries read from input will be clipped to the bbox used by the tool.
+
+    Example:
+    extract stats --in inpolys.shp --out outpolys.geojson --format geojson --clip --classrange 0 5 classified.tif"
+    """
     classes = range(classrange[0], classrange[1] + 1)
 
     raster_reader = MaskedRasterReader(classraster)
