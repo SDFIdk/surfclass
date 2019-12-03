a3f460bce62e30a7512b0ad2c04ce132bb52de2f
AsgerPetersen <asgerpetersen@gmail.com>
2019-12-03 13:36:03 +0100

    rasterwriter: Find usable nodata value

--- src/surfclass/rasterwriter.py
+++ src/surfclass/rasterwriter.py
@@ -16,10 +16,12 @@ def write_to_file(filename, array, origin, resolution, epsg_code, nodata=None):
     ds.SetGeoTransform((originX, resolution, 0, originY, 0, -1 * resolution))
     band = ds.GetRasterBand(1)
 
-    # TODO Handle the case where nodata is none (best guess to avoid removing information)
+    if nodata is None and np.ma.is_masked(array):
+        nodata = find_nodata_value(array)
+
     if nodata is not None:
         band.SetNoDataValue(nodata)
-        if isinstance(array, np.ma.MaskedArray):
+        if np.ma.is_masked(array):
             array = array.filled(fill_value=nodata)
 
     band.WriteArray(array)
@@ -60,3 +62,34 @@ def gdaltype_to_creationoptions(gdaltype):
 def dtype_to_gdaltype(dtype):
     t = str(dtype)
     return map_dtype_gdal[t]
+
+
+def find_nodata_value(a):
+    """Tries to find a usable nodata value
+
+    Args:
+        a (ndarray): 2D ndarray
+
+    Returns:
+        [number]: A number which is not present in the array and which is representable in the array datatype
+    """
+    amin, amax = np.ma.min(a), np.ma.max(a)
+    t = a.dtype
+    if t.kind == "f":
+        tinfo = np.finfo(t)
+    if t.kind in ("i", "u"):
+        tinfo = np.iinfo(t)
+    nines = [-99, -999, -9999, -99999, -999999, -9999999, -99999999, -999999999]
+    n = next(x for x in nines if tinfo.min < x < amin)
+    if n:
+        return n
+    if amin > 0:
+        return 0
+    n = next(-x for x in nines if amax < -x < tinfo.max)
+    if n:
+        return n
+    if tinfo.min < amin:
+        return tinfo.min
+    if tinfo.max > amax:
+        return tinfo.max
+    raise Exception("No suitable nodata value found")
--- tests/test_rasterwriter.py
+++ tests/test_rasterwriter.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from osgeo import gdal
 from surfclass import rasterwriter
+from surfclass.rasterreader import RasterReader
 
 
 def test_writer(tmp_path):
@@ -12,8 +13,8 @@ def test_writer(tmp_path):
     outfile = os.path.join(tmp_path, "test_writer.tif")
     rasterwriter.write_to_file(outfile, data, origin, resolution, epsg)
     assert os.path.exists(outfile)
-    ds = gdal.Open(outfile)
-    assert ds.GetGeoTransform() == (
+    reader = RasterReader(outfile)
+    assert reader.geotransform == (
         origin[0],
         resolution,
         0,
@@ -21,8 +22,58 @@ def test_writer(tmp_path):
         0,
         -1 * resolution,
     )
-    assert ds.RasterXSize == data.shape[1]
-    assert ds.RasterYSize == data.shape[0]
-    band = ds.GetRasterBand(1)
-    assert band.DataType == gdal.GDT_Float32
-    ds = None
+    assert reader.width == data.shape[1]
+    assert reader.height == data.shape[0]
+    assert reader.shape == data.shape
+    read_data = reader.read_raster()
+    assert read_data.dtype == "float32"
+    assert reader.nodata is None
+
+
+def test_writernodata(tmp_path):
+    data = np.arange(1500).astype("float32").reshape((30, 50))
+    data[10:15, 10:15] = 0
+    origin = (550000, 6150000)
+    resolution = 1
+    epsg = 25832
+    outfile = os.path.join(tmp_path, "test_writer_nodata.tif")
+    rasterwriter.write_to_file(outfile, data, origin, resolution, epsg, nodata=0)
+    assert os.path.exists(outfile)
+    reader = RasterReader(outfile)
+    assert reader.geotransform == (
+        origin[0],
+        resolution,
+        0,
+        origin[1],
+        0,
+        -1 * resolution,
+    )
+    assert reader.width == data.shape[1]
+    assert reader.height == data.shape[0]
+    assert reader.shape == data.shape
+    read_data = reader.read_raster()
+    assert read_data.dtype == "float32"
+    assert reader.nodata == 0
+    assert int(np.sum(reader.read_raster(masked=True).mask)) == 26
+
+    masked = np.ma.array(data)
+    masked.mask = data == 0
+    outfile = os.path.join(tmp_path, "test_writer_nodata.tif")
+    rasterwriter.write_to_file(outfile, masked, origin, resolution, epsg)
+    assert os.path.exists(outfile)
+    reader = RasterReader(outfile)
+    assert reader.geotransform == (
+        origin[0],
+        resolution,
+        0,
+        origin[1],
+        0,
+        -1 * resolution,
+    )
+    assert reader.width == data.shape[1]
+    assert reader.height == data.shape[0]
+    assert reader.shape == data.shape
+    read_data = reader.read_raster()
+    assert read_data.dtype == "float32"
+    assert reader.nodata is not None
+    assert int(np.sum(reader.read_raster(masked=True).mask)) == 26
