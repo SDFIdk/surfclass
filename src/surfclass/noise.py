ac7363084fd2ca71069cd0c44868a4ca886d7af0
AsgerPetersen <asgerpetersen@gmail.com>
2019-12-04 13:46:54 +0100

    Brush up noise.py

--- src/surfclass/noise.py
+++ src/surfclass/noise.py
@@ -1,15 +1,26 @@
+"""Tools for handling noisy output from classification."""
 import numpy as np
 from scipy import ndimage as nd
 from skimage.filters import rank
 
 
 def fill_nearest_neighbor(a):
-    """Fills the masked areas with the value of the nearest non-masked cell
-    a: masked array
-    returns: a non-masked ndarray
+    """Fills masked cells with value from nearest non-masked cell.
+
+    Args:
+        a (MaskedArray): A 2D array.
+
+    Raises:
+        TypeError: If `a` is not a MaskedArray
+
+    Returns:
+        ndarray: A 2D array.
+
     """
     if not isinstance(a, np.ma.MaskedArray):
         raise TypeError("Input must be masked array")
+    if not np.ma.is_masked(a):
+        return a
     indexes = nd.distance_transform_edt(
         a.mask, return_indices=True, return_distances=False
     )
@@ -18,10 +29,23 @@ def fill_nearest_neighbor(a):
 
 
 def sieve(a, min_cluster_size, structure=np.ones((3, 3))):
-    """Removes clusters smaller than a threshold size. Inplace
-    a: masked array
-    structure: array_like, optional. Defines feature connections. Default is 8-connectedness
-    min_cluster_size: minimum number of cells to preserve a cluster.
+    """Masks clusters smaller than a threshold size.
+
+    A cluster is a group of cells connected to each other as defined by `structure`.
+
+    Note:
+        Changes input array.
+
+    Args:
+        a (MaskedArray): 2D array.
+        min_cluster_size (int): Minimum size (in number of cells) to keep a cluster. Clusters smaller than
+            this threshold will be masked.
+        structure (ndarray, optional): The neighborhood expressed as a 2-D array of 1’s and 0’s. Defaults to
+            np.ones((3, 3)) which is 8-connectedness.
+
+    Raises:
+        TypeError: If input is not a MaskedArray.
+
     """
     if not isinstance(a, np.ma.MaskedArray):
         raise TypeError("Input must be masked array")
@@ -32,7 +56,20 @@ def sieve(a, min_cluster_size, structure=np.ones((3, 3))):
 
 
 def sieve_mask(a, class_number, min_cluster_size, structure=np.ones((3, 3))):
-    """Gets a mask indicating clusters af given class smaller than a threshold size"""
+    """Gets a bool mask indicating clusters of given cell value smaller than a threshold size.
+
+    Args:
+        a (ndarray): 2D array.
+        class_number (number): Cell value.
+        min_cluster_size (int): Minimum size (in number of cells) to keep a cluster. Clusters smaller than
+            this threshold will be masked.
+        structure (ndarray, optional): The neighborhood expressed as a 2-D array of 1’s and 0’s. Defaults to
+            np.ones((3, 3)) which is 8-connectedness.
+
+    Returns:
+        [ndarray]: 2D array of bools with the same shape as input array.
+
+    """
     class_bin = a == class_number
     labeled_array, _ = nd.measurements.label(class_bin, structure)
     binc = np.bincount(labeled_array.ravel())
@@ -44,23 +81,30 @@ def sieve_mask(a, class_number, min_cluster_size, structure=np.ones((3, 3))):
 
 def majority_vote(a, iterations=1, structure=np.ones((3, 3))):
     """Changes cell values to the most frequent value in its neighborhood.
-    structure: The neighborhood expressed as a 2-D array of 1’s and 0’s.
-    iterations: Number of times to repeat the process.
-    returns a
+
+    Args:
+        a (ndarray): 2D ndarray. Possible a MaskedArray.
+        iterations (int, optional): Number of times to repeat the process. Defaults to 1.
+        structure (ndarray, optional): The neighborhood expressed as a 2-D array of 1’s and 0’s. Defaults to
+            np.ones((3, 3)) which is 8-connectedness.
+
+    Returns:
+        ndarray: 2D ndarray of same dimensions as input array. MaskedArray if input is masked.
+
     """
     nodata = None
     assert a.dtype == "uint8", "Majority vote only works for uint8"
-    if isinstance(a, np.ma.MaskedArray):
+    if np.ma.is_masked(a):
         # windowed_histogram does not work with masked arrays
         nodata = np.max(a) + 1
         a = a.filled(nodata)
     for _ in range(iterations):
         a = rank.windowed_histogram(a, structure).argmax(axis=-1).astype("uint8")
-    return np.ma.masked_values(a, nodata) if not nodata is None else a
+    return np.ma.masked_values(a, nodata) if nodata is not None else a
 
 
 def denoise(a):
-    """Applies simple denoising to a classified raster
+    """Applies simple denoising to a classified raster.
 
     Denoising removes small clusters and fills nodata areas.
 
@@ -69,6 +113,7 @@ def denoise(a):
 
     Returns:
         ndarray: Denoised data
+
     """
     a = majority_vote(a, 2)
     a = fill_nearest_neighbor(a)
