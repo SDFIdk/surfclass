2a212d5d6a32069a6097abc55642fc6a18199a8f
Nicolai Mogensen <nicolaimogensen@outlook.dk>
2019-12-10 15:59:04 +0100

    safer geotransform comparison and cleanup

--- src/surfclass/scripts/prepare.py
+++ src/surfclass/scripts/prepare.py
@@ -130,6 +130,7 @@ def extractfeatures(
     Example:
         surfclass prepare extractfeatures -b 721000 6150000 722000 6151000
             -n 5 -c reflect -f mean -f var 1km_6150_721_amplitude.tif c:\outdir\
+
     """
     # Log inputs
     logger.debug(
