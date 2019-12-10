ac7c5a850529ca27b95abdd0f9e15388735d963b
Nicolai Mogensen <nicolaimogensen@outlook.dk>
2019-12-10 13:04:54 +0100

    fix docstring in classify

--- src/surfclass/classify.py
+++ src/surfclass/classify.py
@@ -6,15 +6,18 @@ from surfclass import rasterio
 def stack_rasters(raster_paths, bbox=None):
     """Convert list of raster paths to arrays and stack them along the 3rd axis.
 
-    if a bbox is supplied the whole stack will be read using that bbox, otherwise the
+    If a bbox is supplied the whole stack will be read using that bbox, otherwise the
     whole raster is read. All rasters must have equal geotransformations to be considered valid.
 
+    Args:
+        raster_paths (list of str): List of paths to feature rasters. The order is important.
+        
     Returns:
-    np.ma.ndarray: Masked 3D ndarray in the form (x,y,n) where n is the raster band
-    np.ndarray: inverted mask, used for retrieving indices of valid cells
-    tuple: Gdal Geotransform (x_min, pixel_size, 0, y_max, 0, -pixel_size)
-    osgeo.osr.SpatialReference: srs Spatial reference system for the output raster (retrived from common srs in raster_paths).
-    tuple: _shape, 2D shape of the resulting output array
+        np.ma.ndarray: Masked 3D ndarray in the form (x,y,n) where n is the raster band
+        np.ndarray: inverted mask, used for retrieving indices of valid cells
+        tuple: Gdal Geotransform (x_min, pixel_size, 0, y_max, 0, -pixel_size)
+        osgeo.osr.SpatialReference: srs Spatial reference system for the output raster (retrived from common srs in raster_paths).
+        tuple: _shape, 2D shape of the resulting output array
 
     """
     features = []
